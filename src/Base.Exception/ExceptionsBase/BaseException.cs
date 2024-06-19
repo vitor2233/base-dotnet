e94f344ba1c9af218ef586afe2dc5157e686112f
vitor2233 <vitorguilherme2233@gmail.com>
2024-06-19 12:03:54 -0300

    Add exception files

---
+++ src/Base.Exception/ExceptionsBase/BaseException.cs
@@ -0,0 +1,9 @@
+﻿namespace Base.Exception.ExceptionBase;
+
+public abstract class BaseException : SystemException
+{
+    protected BaseException(string message) : base(message) { }
+
+    public abstract int StatusCode { get; }
+    public abstract List<string> GetErrors();
+}
---
+++ src/Base.Exception/ExceptionsBase/ErrorOnValidationException.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+
+namespace Base.Exception.ExceptionBase;
+
+public class ErrorOnValidationException : BaseException
+{
+    private readonly List<string> _errors;
+
+    public override int StatusCode => (int)HttpStatusCode.BadRequest;
+
+    public ErrorOnValidationException(List<string> errorMessages) : base(string.Empty)
+    {
+        _errors = errorMessages;
+    }
+
+    public override List<string> GetErrors()
+    {
+        return _errors;
+    }
+}
